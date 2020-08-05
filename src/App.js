6a8d3933bdc3ee6b7d56fc8d39ddbd2e6e883eca
emersoncln <emersoncln@outlook.com.br>
2020-08-05 08:13:57 -0400

    hello world

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+            Hello, world
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprenda React!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
