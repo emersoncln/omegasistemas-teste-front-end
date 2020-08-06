bc504ed14067b3a8f05785c2a0f0f3277e97be6b
emersoncln <emersoncln@outlook.com.br>
2020-08-06 07:38:44 -0400

    app updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import GlobalStyles from "./styles/globalStyles";
 import Header from "./components/header/Header";
 import SearchBox from "./components/searchBox/SearchBox";
+import { Container } from "@material-ui/core";
 
 const App = () => {
   return (
     <>
       <Header />
-      <SearchBox />
-      <GlobalStyles />
+      <Container>
+        <SearchBox />
+        <GlobalStyles />
+      </Container>
     </>
   );
 };
