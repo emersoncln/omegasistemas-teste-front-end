6325dc950fbe7ac9fde422dea16db16bd8c898d5
emersoncln <emersoncln@outlook.com.br>
2020-08-05 23:24:27 -0400

    api updated

--- src/services/api.js
+++ src/services/api.js
@@ -5,13 +5,13 @@ export const api = axios.create({
 });
 
 export const apiAux = () => {
-  return axios.get(
-    "http://www.portaltransparencia.gov.br/api-de-dados/auxilio-emergencial-por-municipio?mesAno=202004&codigoIbge=5107958&pagina=1",
-    {
-      headers: {
-        Accept: "*/*",
-        "chave-api-dados": "a5199d846b3dc8fd798857d25c8682dc",
-      },
-    }
-  );
+  return axios.create({
+    baseURL:
+      "http://www.portaltransparencia.gov.br/api-de-dados/auxilio-emergencial-por-municipio",
+    
+    headers: {
+      Accept: "*/*",
+      "chave-api-dados": "a5199d846b3dc8fd798857d25c8682dc",
+    },
+  });
 };
